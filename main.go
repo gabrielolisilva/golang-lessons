c2333d2f19923bb3763296de7eb378f5e03d549c
Gabriel Silva <gabrielsilva2799@hotmail.com>
2025-02-21 13:20:20 -0300

    FEATURE: Arrays and slices structure addition

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 /* Golang has only one main function in folder scope */
 func main() {
-	testFunction();
+	testFunction(2);
 	testpackage.PackageFunction();
 
 	var n1 int = 10;
@@ -20,9 +20,23 @@ func main() {
 	for x := 0; x < 10; x++ {
 		fmt.Println(x);
 	}
+
+	var agesIlimited [3]int = [3]int{1, 2, 3};
+	agesLimited := []string{"a", "b", "c", "d"};
+	agesLimited = append(agesLimited, "e");
+
+	fmt.Println(agesIlimited, len(agesIlimited), agesLimited, len(agesLimited));
+
+	var slicedAges []string = agesLimited[1:3];
+	var slicedAgesToEnd []string = agesLimited[1:];
+	var slicedAgesBegginingToElement []string = agesLimited[:3];
+	fmt.Println(slicedAges, slicedAgesToEnd, slicedAgesBegginingToElement);
 }
 
-func testFunction() {
-	fmt.Println("Step 1");
-	fmt.Println("Step 2");
+func testFunction(value int) {
+	if value == 1 {
+		fmt.Println("Step 1");
+	} else {
+		fmt.Println("Step 2");
+	}
 }
